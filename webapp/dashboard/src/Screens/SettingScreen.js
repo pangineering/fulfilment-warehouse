187ee938be0bd0ae68a52d5a003ae1285eb92f71
pantkdkrt <p.engineering6415@gmail.com>
2022-06-20 18:41:38 +0700

    Update SettingScreen.js

--- webapp/dashboard/src/Screens/SettingScreen.js
+++ webapp/dashboard/src/Screens/SettingScreen.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import { Container, Button } from "react-bootstrap"
+import React from "react";
+import { Container, Button } from "react-bootstrap";
 
 function SettingScreen() {
   return (
     <Container fluid>
-           <Container>
-      <h1>Setting</h1>
       <Container>
-        <h2>Personal</h2>
-        <Button>Save</Button>
-        <hr />
-      </Container>
-      <Container>
-        <h2>Account</h2>
-        <Button>Save</Button>
-        <hr />
-      </Container>
-      <Container>
-        <h2>Warehouse Detail</h2>
-        <Button>Save</Button>
-        <hr />
+        <h1>Setting</h1>
+        <Container>
+          <h2>Personal</h2>
+          <Button>Save</Button>
+          <hr />
+        </Container>
+        <Container>
+          <h2>Account</h2>
+          <Button>Save</Button>
+          <hr />
+        </Container>
+        <Container>
+          <h2>Warehouse Detail</h2>
+          <Button>Save</Button>
+          <hr />
+        </Container>
       </Container>
     </Container>
-    </Container>
-  )
+  );
 }
 
-export default SettingScreen
+export default SettingScreen;
